372d7263fc2e1a3c16f792fb6ae991f69098aeca
YellTa <>
2024-09-22 20:05:58 +0900

    first commit

---
+++ src/main/java/com/zerospace/zerospace/ZerospaceApplication.java
@@ -0,0 +1,13 @@
+package com.zerospace.zerospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerospaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerospaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerospace/zerospace/ZerospaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerospace.zerospace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerospaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
