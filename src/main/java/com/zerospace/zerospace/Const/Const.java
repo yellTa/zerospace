453c0d96c4ed14f379717fd87e6930d6228df843
YellTa <>
2024-10-14 00:28:56 +0900

    chore : application.properties에 상수 설정하기 상수를 설정하고 properties파일을 숨김

--- src/main/java/com/zerospace/zerospace/Const/Const.java
+++ src/main/java/com/zerospace/zerospace/Const/Const.java
@@ -1,10 +1,21 @@
 package com.zerospace.zerospace.Const;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Const {
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 20 * 60;
     public static final long REFRESH_TOKEN_VALIDITY_SECONDS = 30 * 24 * 60 * 60;
 
-    public static final String SECRET_KEY = "zerosapceOur256SpecialKeyShouldBeVerySecure";
-    public static final String ACCESS_TOKEN_NAME = "Authorization";
-    public static final String REFRESH_TOKEN_NAME = "refreshToken";
+    @Value("${jwt.secret-key}")
+    private String secretKeyValue;
+
+    @Value("${jwt.access-token-name}")
+    private String accessTokenNameValue;
+
+    @Value("${jwt.refresh-token-name}")
+    private String refreshTokenNameValue;
+
+    public static String SECRET_KEY;
+    public static String ACCESS_TOKEN_NAME;
+    public static String REFRESH_TOKEN_NAME;
 }
