df80cf66eb44b5341e31743a223d7b8bc2d9a116
YellTa <>
2024-10-16 16:18:07 +0900

    api연동으로 바뀐 것들

--- src/main/java/com/zerospace/zerospace/Config.java
+++ src/main/java/com/zerospace/zerospace/Config.java
@@ -2,14 +2,12 @@ package com.zerospace.zerospace;
 
 
 import com.zerospace.zerospace.filter.CORSFilter;
-import com.zerospace.zerospace.filter.JwtAuthFilter;
 import com.zerospace.zerospace.handler.OAuthSuccessHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,11 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @Slf4j
 public class SecurityFilter {
-    private final JwtAuthFilter jwtAuthFilter;
     private final CORSFilter corsFilter;
 
-    public SecurityFilter(JwtAuthFilter jwtAuthFilter, CORSFilter corsFilter) {
-        this.jwtAuthFilter = jwtAuthFilter;
+    public SecurityFilter(CORSFilter corsFilter) {
         this.corsFilter = corsFilter;
     }
 
@@ -30,7 +26,6 @@ public class SecurityFilter {
 
         httpSecurity
                 .addFilterBefore(corsFilter, OAuth2LoginAuthenticationFilter.class)
-                .addFilterBefore(jwtAuthFilter, OAuth2LoginAuthenticationFilter.class)
                 .authorizeRequests(authorize -> authorize
                         // 로그인 없이 접근 가능한 URI 설정
                         .requestMatchers("/calendar/data", "/resources/**", "/static/**", "/login", "/").permitAll()
@@ -38,10 +33,8 @@ public class SecurityFilter {
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .loginPage("https://zzerospace.store:8080/oauth2/authorization/kakao")
-                        .successHandler((request, response, authentication) -> {
-                            response.sendRedirect("/loginResult");
-                        })
+                        .loginPage("https://zzerospace.store:8443/oauth2/authorization/kakao")
+                        .successHandler(successHandler)
                 );
         return httpSecurity.build();
     }
